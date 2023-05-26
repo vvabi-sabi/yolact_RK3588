f110406568c307a5618e0789f5a31813241ba88d
BelotserkovskiyVA <v.belotserkovskii@g.nsu.ru>
2023-05-26 15:39:08 +0700

    added the class Camera

---
+++ base/camera.py
@@ -0,0 +1,47 @@
+import cv2
+
+import pre_processes
+
+class Camera():
+    """
+    """
+    def __init__(self, source: int, queue, model_list: list):
+        self._queue = queue
+        self.source = source
+        self.model_list = model_list
+        self._count = 0
+        self._begin = 0
+
+    def _bgr2rgb(self, frame):
+        frame = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2RGB)
+        return frame
+
+    def _bgr2gray(self, frame):
+        frame = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2GRAY)
+        return frame
+
+    @property
+    def frames(self):
+        cap = cv2.VideoCapture(self.source)
+        if(not cap.isOpened()):
+            print("Bad source")
+            raise SystemExit
+        try:
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    print("Camera stopped!")
+                    raise SystemExit
+                yield frame
+            cap.release()
+        except Exception as e:
+            print("Stop recording loop. Exception {}".format(e))
+
+    def pre_process(self):
+        for frame in self.frames:
+            rgb_frame = self._bgr2rgb(frame)
+            gray_frame = self._bgr2gray(frame)
+            self._queue.put((frame, [rgb_frame, gray_frame]))
+    
+    def get_frame(self):
+        return next(self.frames)
