20c4c27b1e2f05aea4030846642d045ad66bed86
BelotserkovskiyVA <v.belotserkovskii@g.nsu.ru>
2023-07-03 15:52:52 +0700

    added docstring

--- base/camera.py
+++ base/camera.py
@@ -3,9 +3,45 @@ from multiprocessing import Process
 
 
 class Camera(Process):
+    """This class represents a camera process that captures frames from a video source 
+    and performs various operations on the frames (preprocess).
+
+    Attributes
+    ----------
+    net_size : tuple
+        The size of the neural network input.
+    queue : Queue
+        The queue to put the processed frames into.
+    source : int, str
+        The video source (also path to file.mp4) to capture frames from.
+    frames : generator
+        A generator object that yields frames from the video capture.
+
+    Methods
+    -------
+    get_frame(None)
+        Returns the next frame from the frames generator.
+    resize_frame(frame, net_size)
+        Resizes the given frame using OpenCV's resize function.
+    crop_frame(frame, net_size)
+        Crops the given frame based on net_size.
+    run(None) 
+        Iterates over the frames generator, processes each frame, and puts it into the queue.
+
     """
-    """
+
     def __init__(self, source: int, queue, onnx=True):
+        """
+        Parameters
+        ----------
+        source : int, str
+            The video source.
+        queue : Queue
+            The queue in which processed frames are placed. Then these frames will be fed 
+            to the input of the neural network.
+        onnx : bool, optional
+            Whether to use ONNX model for postprocessing. Defaults to True.
+        """
         super().__init__(group=None, target=None, name=None, args=(), kwargs={}, daemon=True)
         INPUT_SIZE = (550 if onnx else 544)
         self.net_size = (INPUT_SIZE, INPUT_SIZE) 
@@ -14,6 +50,22 @@ class Camera(Process):
 
     @property
     def frames(self):
+        """
+        Parameters
+        ----------
+        self : Camera
+            The instance of the Camera class.
+        
+        Returns
+        -------
+        frame : np.ndarray
+            The next frame from the video source.
+        
+        Raises
+        ------
+        SystemExit
+            If the video source does not open or if the frame cannot be read.
+        """
         cap = cv2.VideoCapture(self.source)
         #cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         if(not cap.isOpened()):
@@ -32,9 +84,29 @@ class Camera(Process):
             print(f"Stop recording loop. Exception {e}")
     
     def get_frame(self):
+        """It yields the frame, making it available for further processing outside the function.
+        
+        Returns
+        -------
+        next(frames) : np.ndarray
+        """
         return next(self.frames)
     
     def resize_frame(self, frame, net_size):
+        '''This function resizes a frame to a specified size.
+        
+        Parameters
+        ----------
+        frame : np.ndarray
+            The input frame to be resized.
+        net_size : tuple
+            The desired size of the output frame.
+        
+        Returns
+        -------
+        resized_frame : np.ndarray
+            The resized frame.
+        '''
         frame_size = frame.shape[:2]
         if any(map(lambda x,y: x < y, frame_size, net_size)):
             return cv2.resize(frame, net_size, interpolation = cv2.INTER_CUBIC)
@@ -42,6 +114,23 @@ class Camera(Process):
             return cv2.resize(frame, net_size, interpolation = cv2.INTER_AREA)
 
     def crop_frame(self, frame, net_size):
+        '''This function crops a frame to a specified size.
+        
+        It calculates the top-left coordinates of the crop area by subtracting half of the desired size 
+        from the center coordinates of the frame.
+        
+        Parameters
+        ----------
+        frame : np.ndarray
+            The input frame to be cropped.
+        net_size : tuple
+            The desired size of the output frame.
+        
+        Returns
+        -------
+        cropped_frame : np.ndarray
+            The cropped frame.
+        '''
         net_size = net_size[0]
         hc, wc = frame.shape[0]/2, frame.shape[1]/2
         h0, w0 = int(hc-net_size/2), int(wc-net_size/2)
@@ -49,6 +138,17 @@ class Camera(Process):
         return frame[h0:(h0+net_size), w0:(w0+net_size)]
 
     def run(self):
+        '''When processing a raw frame, there are two methods to choose from:
+        resize_frame or crop_frame.
+        
+        Parameters
+        ----------
+        self : Camera
+        
+        Returns
+        -------
+        None
+        '''
         for raw_frame in self.frames:
             #frame = self.resize_frame(raw_frame, self.net_size) #cv2.resize(raw_frame.copy(), self.net_size)
             frame = self.crop_frame(raw_frame, self.net_size)
